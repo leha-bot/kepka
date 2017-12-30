2425691e2ecc2e80f3a885f20d3ccdae30807461
Berkus Decker <berkus@madfire.net>
2017-12-31 00:59:27 +0200

    [temp] verbose build

--- .travis/build.sh
+++ .travis/build.sh
@@ -3,5 +3,5 @@
 set -x
 
 cmake -G Ninja -DCMAKE_BUILD_TYPE=$BUILD_TYPE $EXTRA_CMAKE_FLAGS -DBUILD_TESTING=ON .. || exit 1
-cmake --build . || exit 1
+cmake --build . -- -v || exit 1
 ASAN_OPTIONS=alloc_dealloc_mismatch=0 ctest . || exit 1
